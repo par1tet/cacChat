d0cb9d5fcdcaa2c58450419ed9852df3739d4bdd
S7b0t4 <toni.paun.00@mail.ru>
2024-09-11 21:24:55 +0500

    first commit on front branch

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SignUpDesign from "./pages/sign-up/SignUpDesign"
+
+function App() {
+
+  return (
+    <>
+      <SignUpDesign/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/sign-up/SignUpDesign.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function SignUpDesign({}: Props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignUpDesign
