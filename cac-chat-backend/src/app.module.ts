8005c4e70d0ac5b5dcd1b43251dd389f937c0ba8
par1tet <mankova486@gmail.com>
2024-09-12 10:52:55 +0300

    postgre update
    feat: create table of users
    feat: postgres config move in .env file

--- cac-chat-backend/src/app.module.ts
+++ cac-chat-backend/src/app.module.ts
@@ -4,22 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './users/users.model';
 
 @Module({
-	imports: [SequelizeModule.forRoot({
+	imports: [ConfigModule.forRoot({
+		envFilePath: `./.${process.env.NODE_ENV}.env`
+	}),
+	SequelizeModule.forRoot({
 		dialect: 'postgres',
-		host: 'localhost',
-		port: 5432,
-		username: 'postgres',
-		password: 'root',
-		database: 'nest-back',
-		models: [],
+		host: process.env.POSTGRES_HOST,
+		port: +process.env.POSTGRES_PORT,
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DB,
+		models: [User],
 		autoLoadModels: true
 	}),
-	ConfigModule.forRoot({
-		envFilePath: './.env'
-	})
-	,UsersModule],
+	UsersModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
---
+++ cac-chat-backend/src/users/users.model.ts
@@ -0,0 +1,22 @@
+import { Column, Table, DataType, Model } from "sequelize-typescript";
+
+interface UserCreateAttrs {
+    email: string,
+    password: string,
+    nickname: string
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User> {
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @Column({type: DataType.STRING, unique: true})
+    nickname: string;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+
+    @Column({type: DataType.STRING, unique: true})
+    password: string;
+}
