02bdd5928cc44fc7e17e82345cc0a7f67c3e4ee1
par1tet <mankova486@gmail.com>
2024-09-13 22:13:33 +0300

    feat: add save token on localStorage

--- cac-chat-front/src/app/test-page/TestPage.tsx
+++ cac-chat-front/src/app/test-page/TestPage.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
 import cl from "./TestPage.module.css"
 
 type Props = {}
 
 export default function TestPage({}: Props) {
-  return (
-    <ul className={cl['test_page_list']}>
-      <a href="/sign_up">sign_up</a>
-      <a href="/sign_in">sign_in</a>
-      <a href="/chats">chats</a>
-    </ul>
-  )
+	const [token] = useState(localStorage.getItem('token'))
+
+	console.log(localStorage.getItem('token'))
+	
+	if(token){
+		return (
+			<ul className={cl['test_page_list']}>
+				<a href="/chats">chats</a>
+			</ul>
+		)
+	}else{
+		return (
+			<ul className={cl['test_page_list']}>
+				<a href="/sign_up">sign_up</a>
+				<a href="/sign_in">sign_in</a>
+			</ul>
+		)
+	}
 }
